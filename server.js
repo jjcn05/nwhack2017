6bcd052a8e68c3bd96ec8940a4b9e3b7f6bb7a0d
lexynyx <jjcn05@hotmail.com>
2017-03-19 07:01:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,23 +4,23 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('hello world')
    // res.sendFile('/site/index.html');
-});
+})
 
 app.post('/', function (req, res) {
     res.send('POST Request');
-});
+})
 
 app.put('/', function (req, res) {
     res.send('PUT Request');
-});
+})
 
 app.delete('/', function (req, res) {
     res.send('DELETE Request');
-});
+})
 
 var server = app.listen(5000, function () {
     console.log('Node server is running..');
-});
+})
    
 //app.use("/site" , express.static(__dirname + 'index')); //Serves resources from public folder , access site folder then looks for index file
 
