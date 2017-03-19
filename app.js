0747c472e558c7dff9c17d4d2c9da7b6de4bbdd7
lexynyx <jjcn05@hotmail.com>
2017-03-19 01:52:38 -0700

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-{
-    "name": "Meal Plan Generator",
-    "description": "nwHacks 2017 Meal Plan Generator",
-    "repository": "https://github.com/lexynyx/nwhack2017",
-    "keywords": ["node", "express", "react", "meal plan"]
-}
