db0da3287f8ca5a5cecc4e7cd57bb712ffdc5687
lexynyx <jjcn05@hotmail.com>
2017-03-19 05:52:13 -0700

    Add files via upload

---
+++ site/app.js
@@ -0,0 +1,4 @@
+var React = require('react');
+var Meals = require('./views/index.jsx');
+
+// render ...
---
+++ site/views/index.jsx
@@ -0,0 +1,61 @@
+class Meals extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calorie: 0,
+      price: 0
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleSubmit(event){
+     alert('Informations are submitted');
+     event.preventDefault();
+
+     var data = {
+        calorie : this.ref.calorie.getDOMNode().value,
+        price : this.ref.price.getDOMNode().value,
+     }
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <h1>Calorie Randomizer for Everyone </h1>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Calorie:
+          <input
+            name="calorie"
+            type="number"
+            ref="calorie"
+            value={this.state.calorie}
+            onChange={this.handleInputChange} />
+        </label>
+        <br /><br/>
+        <label>
+          Price:
+          <input
+            name="price"
+            type="number"
+            ref="price"
+            value={this.state.price}
+            onChange={this.handleInputChange} />
+        </label><br/><br/>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Meals />,
+  document.getElementById('root')
+);
