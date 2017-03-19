50d0fd0f1c842f630bff6877921a939c0660ec4d
lexynyx <jjcn05@hotmail.com>
2017-03-19 07:20:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var app = express();
 app.use(express.static(__dirname + 'site')); //Serves resources from public folder
 
 // respond with index.html when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.render('/site/index.html'); // accress index.html in the site folder
-});
+//app.get('/', function(req, res) {
+//  res.render('/site/index.html'); // accress index.html in the site folder
+//});
 
 var server = http.createServer(app); // register routes to app variable
 server.listen(process.env.PORT || 5000);
