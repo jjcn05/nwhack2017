9cd472604bcc0985e60f6aa8cf405927dd71dd73
lexynyx <jjcn05@hotmail.com>
2017-03-19 06:26:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.use("/site" , express.static(__dirname + 'index')); //Serves resources from public folder , access site folder then looks for index file
+//app.use("/site" , express.static(__dirname + 'index')); //Serves resources from public folder , access site folder then looks for index file
 
-var server = app.listen(process.env.PORT || 5000);
+//var server = app.listen(process.env.PORT || 5000);
+
+
+app.get('/site', function(req, res){
+  res.render('index');
+});
+
+app.listen(3000);
