023e022da075c76596b904a872f564eeca0d651f
lexynyx <jjcn05@hotmail.com>
2017-03-19 07:01:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var app = express();
 
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-
 app.get('/', function (req, res) {
     res.send('hello world')
    // res.sendFile('/site/index.html');
