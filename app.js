c204cbe7f82fa3bf4744f1bb650984b2582aada1
lexynyx <jjcn05@hotmail.com>
2017-03-19 00:02:59 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+{
+    "name": "Meal Plan Generator",
+    "description": "nwHacks 2017 Meal Plan Generator",
+    "repository": "https://github.com/lexynyx/nwhack2017",
+    "keywords": ["node", "express", "react", "meal plan"]
+}
