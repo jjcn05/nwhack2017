1445aad4038a9523c080c415b131621254dce57c
lexynyx <jjcn05@hotmail.com>
2017-03-19 07:08:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,27 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+app.use("/site" , express.static(__dirname + 'index')); //Serves resources from public folder , access site folder then looks for index file
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
-
-//app.use("/site" , express.static(__dirname + 'index')); //Serves resources from public folder , access site folder then looks for index file
-
-//var server = app.listen(process.env.PORT || 5000);
+var server = app.listen(process.env.PORT || 5000);
 
