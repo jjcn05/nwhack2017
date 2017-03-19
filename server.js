2634645cf8c474618b9057ede874de0c1e0d57dc
lexynyx <jjcn05@hotmail.com>
2017-03-19 06:43:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('<html><body><h1>Hello World</h1></body></html>');
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/site', function (req, res) {
+    res.sendFile('index.html');
 });
 
-app.post('/submit-data', function (req, res) {
+app.post('/', function (req, res) {
     res.send('POST Request');
 });
 
-app.put('/update-data', function (req, res) {
+app.put('/', function (req, res) {
     res.send('PUT Request');
 });
 
-app.delete('/delete-data', function (req, res) {
+app.delete('/', function (req, res) {
     res.send('DELETE Request');
 });
 
